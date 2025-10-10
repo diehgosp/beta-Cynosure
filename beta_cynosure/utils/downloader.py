0ac0b6a6b9360d58eef44f8f79dbf9aeb22b2d0b
Diego <162825844+diehgosp@users.noreply.github.com>
2025-10-10 20:01:35 -0300

    Delete beta_cynosure directory

--- beta_cynosure/utils/downloader.py
+++
@@ -1,8 +0,0 @@
-import io
-import requests
-import zipfile
-
-def download_zip(url: str) -> zipfile.ZipFile:
-    response = requests.get(url)
-    response.raise_for_status()
-    return zipfile.ZipFile(io.BytesIO(response.content))
